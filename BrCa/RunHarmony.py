ca0db2c2100fd47c8c3eaa749616f379c6501261
Zhang <luke.lq.zhang@gmail.com>
2023-11-21 20:46:02 +0000

    scoring scimap accuracy

--- BrCa/RunHarmony.py
+++ BrCa/RunHarmony.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 
 adata = ad.read_h5ad('/mnt/plummergrp/QuPath_0.4.4/BrCa/session_file.h5ad')
-adata.obs['batch'] = pd.Categorical(adata.obs['batch'])
+adata.obs['batch'] = pd.Categorical(adata.obs['ImageID'])
 # adata.obs['batch'] = adata.obs['batch'].cat.rename_categories({'0': 'sample1', '1': ' sample2'})
 sc.tl.pca(adata)
 sce.pp.harmony_integrate(adata, 'batch')
