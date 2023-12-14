e31aefd09d004e170a863587d432dc8597a18cb9
Zhang <luke.lq.zhang@gmail.com>
2023-12-14 15:17:17 +0000

    manual clean up for data

--- BrCa/RunHarmony.py
+++ BrCa/RunHarmony.py
@@ -4,7 +4,7 @@ import anndata as ad
 import pandas as pd
 import os
 
-adata = ad.read_h5ad('/mnt/plummergrp/QuPath_0.4.4/BrCa/session_file.h5ad')
+adata = ad.read_h5ad('/mnt/plummergrp/QuPath_0.4.4/BrCa/session_file_clr.h5ad')
 adata.obs['batch'] = pd.Categorical(adata.obs['ImageID'])
 # adata.obs['batch'] = adata.obs['batch'].cat.rename_categories({'0': 'sample1', '1': ' sample2'})
 sc.tl.pca(adata)
